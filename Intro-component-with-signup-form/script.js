7d052af5d11cde6159e6ed82a7f86013375d94bd
ibuild-webdev <hateios12@gmail.com>
2021-05-04 19:34:16 +0530

    "Made changes to js file"

--- Intro-component-with-signup-form/script.js
+++ Intro-component-with-signup-form/script.js
@@ -5,12 +5,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
     form.addEventListener('submit',(e)=>{
         for(let i=0;i<inputs.length;i++){
             if(inputs[i].value===""){
-                console.log(inputs[i].value);
                 span_p[i].classList.remove("invisible");
                 e.preventDefault();
             }
             else{
-                span_p[i].classList.add("invisible");
+                if(i==2){
+                    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(inputs[i].value)){
+                        span_p[i].classList.remove("invisible");
+                        e.preventDefault();
+                    }    
+                    else{
+                        span_p[i].classList.add("invisible");
+                    }
+                }
+                else{
+                    span_p[i].classList.add("invisible");
+                }
 
             }
         }
