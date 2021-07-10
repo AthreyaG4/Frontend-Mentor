562dfa17f80e29a2748003967fb9eeca191c3cde
ibuild-webdev <hateios12@gmail.com>
2021-07-10 11:13:39 +0530

    adding files

---
+++ ping-coming-soon-page/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    form = document.querySelector(form);
+    input = document.querySelector(input);
+    paragraph = document.querySelector("form p");
+
+});
