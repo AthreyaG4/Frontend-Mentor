e2a260c206d3cc09e65ea3f9b9707866431a698e
Your Nameibuild-webdev <hateios12@gmail.com>
2021-07-10 11:53:49 +0530

    Adding files

--- ping-coming-soon-page/script.js
+++ ping-coming-soon-page/script.js
@@ -1,6 +1,27 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    form = document.querySelector(form);
-    input = document.querySelector(input);
-    paragraph = document.querySelector("form p");
-
+    form = document.querySelector("form");
+    input = document.querySelector("input");
+    paragraph_mobile = document.querySelector(".error-mobile");
+    paragraph_desktop = document.querySelector(".error-desktop");
+    form.addEventListener('submit',(e)=>{
+        if(input.value===""){
+            e.preventDefault();
+            paragraph_desktop.classList.remove("invisible");
+            paragraph_mobile.classList.remove("invisible");
+            input.classList.add("red-border");
+        }
+        else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input.value)){
+            e.preventDefault();
+            paragraph_desktop.classList.remove("invisible");
+            paragraph_mobile.classList.remove("invisible");
+            input.classList.add("red-border");
+            
+        }
+        else{
+            paragraph_desktop.classList.add("invisible");
+            paragraph_mobile.classList.add("invisible");
+            input.classList.remove("red-border");
+            e.preventDefault();
+        }
+    })
 });
