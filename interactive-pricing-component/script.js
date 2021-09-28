7dfb7879bd0a6786ef6f8c5263f19c2f0fc12a73
ibuild-webdev <hateios12@gmail.com>
2021-09-28 18:47:39 +0530

    made some changes

---
+++ interactive-pricing-component/script.js
@@ -0,0 +1,32 @@
+const toggle_button = document.querySelector(".toggle");
+const ball = document.querySelector(".ball");
+
+toggle_button.addEventListener("click",()=>{
+    if(ball.classList.contains("right")){
+        ball.classList.remove("right");
+    }
+    else{
+        ball.classList.add("right");
+    }
+
+    pageviews.innerHTML = pageview_values[slider.value-1];
+    price.innerHTML = `$${price_values[slider.value-1].toFixed(2)}`;
+    if(ball.classList.contains("right")){
+        price.innerHTML = `$${(price_values[slider.value-1] - (price_values[slider.value-1]*25/100)).toFixed(2)}`;
+    }
+});
+
+const pageviews = document.querySelector(".views");
+const price = document.querySelector(".price");
+const slider = document.querySelector(".slider");
+
+let pageview_values = ['10k','50k','100k','500k','1M'];
+let price_values = [8,12,16,24,36];
+
+slider.oninput = () =>{
+    pageviews.innerHTML = pageview_values[slider.value-1];
+    price.innerHTML = `$${price_values[slider.value-1].toFixed(2)}`;
+    if(ball.classList.contains("right")){
+        price.innerHTML = `$${(price_values[slider.value-1] - (price_values[slider.value-1]*25/100)).toFixed(2)}`;
+    }
+}
