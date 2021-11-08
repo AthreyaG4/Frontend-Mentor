9381151e3f1f1d702e6af771a7fe1252e59c8486
ibuild-webdev <hateios12@gmail.com>
2021-11-08 22:22:48 +0530

    made some changes

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -4,6 +4,8 @@ const radio_btns = document.querySelectorAll("input[type='radio']");
 const continue_btns = document.querySelectorAll(".continue-btn");
 const close_btn_modal = document.querySelector(".close-icon-modal"); 
 const inputs = document.querySelectorAll(".selection-modal input[type=text]");
+const hamburger = document.querySelector(".icon-hamburger");
+const cross_icon = document.querySelector(".icon-close");
 
 const overlay = document.querySelector(".overlay");
 const selection_modal = document.querySelector(".selection-modal");
@@ -23,17 +25,27 @@ const selection_modal_number_left = document.querySelectorAll(".selection-modal 
 const total_collection = document.querySelector(".total-collection #bold-text");
 const total_backers = document.querySelector(".total-backers #bold-text");
 
-console.log(total_collection);
-console.log(total_backers);
+//Interaction for hamburger and cross-icon in mobile menu
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.add("invisible");
+    cross_icon.classList.remove("invisible");
+});
 
+cross_icon.addEventListener("click",()=>{
+    hamburger.classList.remove("invisible");
+    cross_icon.classList.add("invisible");
+});
+
+//Setting the first values for the number of stands left for each type. Uses the number left array
 main_number_left.forEach((ele,index) =>{
     ele.textContent = number_left[index];
 });
 
 selection_modal_number_left.forEach((ele,index) =>{
     ele.textContent = number_left[index];
-})
+});
 
+//Interaction for all the buttons
 back_this_prj_btn.addEventListener("click",() => {
     overlay.classList.remove("hidden");
     selection_modal.classList.remove("hidden");
@@ -42,18 +54,22 @@ back_this_prj_btn.addEventListener("click",() => {
 
 continue_btns.forEach((continue_btn,index) => {
     continue_btn.addEventListener("click",() => {
-        selection_modal.classList.add("hidden");
-        success_modal.classList.remove("hidden");
-        if(index!=0){
-            number_left[index-1]--;
-            main_number_left[index-1].textContent = `${number_left[index-1]} `;
-            selection_modal_number_left[index-1].textContent = `${number_left[index-1]}`;
+        if(!isNaN(inputs[index].value)){
+            if((index==1 && inputs[index].value>=25)||(index==2 && inputs[index].value>=75)||(index==3 && inputs[index].value>=200)||(index==0)){
+                selection_modal.classList.add("hidden");
+                success_modal.classList.remove("hidden");
+                if(index!=0){
+                    number_left[index-1]--;
+                    main_number_left[index-1].textContent = `${number_left[index-1]} `;
+                    selection_modal_number_left[index-1].textContent = `${number_left[index-1]}`;
+                }
+            }
         }
         inputs.forEach(input =>{
             input.value = "";
         });
     });
-})
+});
 
 select_btns.forEach((select_btn,index) =>{
     select_btn.addEventListener("click",()=>{
@@ -90,8 +106,9 @@ bookmark_btn.addEventListener("click",()=>{
     else{
         bookmark_text.textContent = "Bookmark";
     }
-})
+});
 
+//Check for disabling the card using the number left in each type
 setInterval(()=>{
     number_left.forEach((number,index) =>{
         if(number == 0){
@@ -104,9 +121,3 @@ setInterval(()=>{
         }
     })
 },100);
-
-
-
-
-
-
