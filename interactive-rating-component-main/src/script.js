dd331e2f6482fd0cb3c2745449a13d6e69b7668b
Your Nameibuild-webdev <hateios12@gmail.com>
2022-03-27 14:57:41 +0530

    completed

---
+++ interactive-rating-component-main/src/script.js
@@ -0,0 +1,24 @@
+const labels = document.querySelectorAll(".buttons label");
+const radios = document.querySelectorAll("input[type=radio]");
+const submitButton = document.querySelector(".submit-button");
+const rating = document.querySelector(".rating");
+const main = document.querySelector("main");
+const thankyou = document.querySelector(".thankyou-card");
+
+submitButton.addEventListener("click",(e)=>{
+    e.preventDefault();
+    radios.forEach((radio,index) => {
+        if(radio.checked){
+            rating.textContent = index+1;
+        }
+    });
+    main.classList.add("invisible");
+    thankyou.style.display = "flex";
+    thankyou.classList.remove("invisible");
+});
+
+setInterval(()=>{
+    radios.forEach((radio,index) => {
+        radio.checked ? labels[index].classList.add("selected") : labels[index].classList.remove("selected");
+    });
+},100);
